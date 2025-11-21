2291d39223108505ed971677d0d52f72a12d584f
nwanodicv <victornwanodi@gmail.com>
2025-11-21 19:44:07 +0100

    update

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,13 +1,17 @@
-//const getString = window.location.search;
-//console.log(getString);
+const getString = window.location.search;
+console.log(getString);
 
 myInfo = new URLSearchParams(window.location.search);
-//console.log(myInfo);
+console.log(myInfo);
 
 document.querySelector('#results').innerHTML = `
-<p>Your rating is: ${myInfo.get('rating')} </p>
-<p>installation Date: ${myInfo.get('installationDate')} </p>
-<p>Your comment is: ${myInfo.get('reviewText')} </p>
-<p>Your username is: ${myInfo.get('userName')} </p>
-<p>Your features: ${myInfo.get('features')} </p>
+<p><strong>Your First Name:</strong> ${myInfo.get('firstname')} </p>
+<p><strong>Your Last Name: </strong>${myInfo.get('lastName')} </p>
+<p><strong>Organizational Title:</strong> ${myInfo.get('organizationalTitle')} </p>
+<p><strong>Your Email:</strong> ${myInfo.get('email')} </p>
+<p><strong>Phone Number:</strong> ${myInfo.get('phone')} </p>
+<p><strong>Your Business Name:</strong> ${myInfo.get('businessName')} </p>
+<p><strong>Your Membership Level:</strong> ${myInfo.get('MembershipLevel')} </p>
+<p><strong>Business Description</strong>: ${myInfo.get('businessDescription')} </p>
+<p><strong>You joined the Ogwashi-uku Chamber of Commerce on the ${myInfo.get('timestamp')} </strong></p>
 `;
