c815311564c61990604e18eebbe26a84e709f2ba
nwanodicv <victornwanodi@gmail.com>
2025-12-08 01:59:58 +0100

    update

--- finalproject/scripts/vjutech-thankyou.js
+++ finalproject/scripts/vjutech-thankyou.js
@@ -1,14 +1,14 @@
 //const getString = window.location.search;
-//console.log(getString)
+//console.log(getString
+const myInfo = new URLSearchParams(window.location.search);
+//console.log(myInfo);
 
-myInfo = new URLSearchParams(window.location.search);
-console.log(myInfo);
-
+//Display the values on the thank you page 
 document.querySelector('#results').innerHTML = `
 <p><strong>Your First Name:</strong> ${myInfo.get('firstname')} </p>
 <p><strong>Your Last Name: </strong>${myInfo.get('lastName')}</p>
 <p><strong>Your Email:</strong> ${myInfo.get('email')} </p>
 <p><strong>Phone Number:</strong> ${myInfo.get('phone')} </p>
-<p><strong>Your Business Name:</strong> ${myInfo.get('homeaddress')} </p>
+<p><strong>Your contact Info:</strong> ${myInfo.get('homeaddress')} </p>
 <p><strong>You signed Up on the ${myInfo.get('timestamp')} </strong></p>
 `;
